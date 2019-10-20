46df3e0e3d98273160e44eee43e7127b3120e1ee
Danielshe <shekwancheung0528@gmail.com>
2019-10-20 16:54:24 +0800

    Resolve issues
    Bump Version to 0.2.0, Move inject point, remove width and height, javadoc

--- fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/api/event/client/HudRenderCallback.java
+++ fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/api/event/client/HudRenderCallback.java
@@ -4,13 +4,19 @@ import net.fabricmc.fabric.api.event.Event;
 import net.fabricmc.fabric.api.event.EventFactory;
 
 public interface HudRenderCallback {
+	/** Called after rendering the status effects overlay when rendering the hud. */
 	public static final Event<HudRenderCallback> EVENT = EventFactory.createArrayBacked(HudRenderCallback.class,
-		listeners -> (screenWidth, screenHeight, delta) -> {
+		listeners -> delta -> {
 			for (HudRenderCallback event : listeners) {
-				event.render(screenWidth, screenHeight, delta);
+				event.render(delta);
 			}
 		}
 	);
 
-	void render(int screenWidth, int screenHeight, float delta);
+	/**
+	 * Called when the hud is rendered, which is displayed in game, in a world.
+	 *
+	 * @param delta the number of ticks passed since the last tick
+	 */
+	void render(float delta);
 }
--- fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/mixin/eventslifecycle/MixinInGameHud.java
+++ fabric-events-lifecycle-v0/src/main/java/net/fabricmc/fabric/mixin/eventslifecycle/MixinInGameHud.java
@@ -1,23 +1,18 @@
 package net.fabricmc.fabric.mixin.eventslifecycle;
 
 import net.fabricmc.fabric.api.event.client.HudRenderCallback;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class MixinInGameHud {
-	@Shadow
-	@Final
-	private MinecraftClient client;
 
-	@Inject(method = "render", at = @At("RETURN"))
+	@Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderStatusEffectOverlay()V", shift = At.Shift.AFTER))
 	public void render(float delta, CallbackInfo callbackInfo) {
-		HudRenderCallback.EVENT.invoker().render(client.window.getScaledWidth(), client.window.getScaledHeight(), delta);
+		HudRenderCallback.EVENT.invoker().render(delta);
 	}
+
 }
